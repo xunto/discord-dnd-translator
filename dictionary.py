4f1ae6aaea54125620376916b9f470a878fe987e
xunto <hunterdelyx1@gmail.com>
2017-09-08 16:56:24 +0300

    Prepare dictionary.py for testing

--- dictionary.py
+++ dictionary.py
@@ -14,35 +14,45 @@ class Dictionary:
     """
     Responsible for fetch and search of translations.
     """
+    INDEX_PATH = ".index"
 
-    def __update_index_csv(self, filename) -> None:
+    def __update_index(self, data) -> None:
         """
-        Read spell names from specified file.
-        :param filename: Filename.
+        Update index with specified data.
+        :param data: Data.
         :return: Nothing.
         """
         writer = self.index.writer()
-        with open(filename, 'r', encoding='utf-8') as csv_file:
-            reader = csv.reader(csv_file)
-            for i, row in enumerate(reader):
-                for variant in row:
-                    writer.add_document(
-                        group=i,
-                        content=variant
-                    )
+        for i, row in enumerate(data):
+            for variant in row:
+                writer.add_document(
+                    group=i,
+                    content=variant
+                )
         writer.commit()
 
-    def __init_index(self, index_dir):
+    def __init_index(self):
         # TODO: Add a way to clear index
-        #if os.path.exists(index_dir):
-        #    self.index = index.open_dir
-        #else:
+        # if os.path.exists(index_dir):
+        #     self.index = index.open_dir
+        # else:
         #    os.mkdir(index_dir)
-        self.index = index.create_in(index_dir, DictionaryRecord)
+        self.index = index.create_in(Dictionary.INDEX_PATH, DictionaryRecord)
 
-    def __init__(self):
-        self.__init_index("index")
-        self.__update_index_csv("spells.csv")
+    def __init__(self, data):
+        self.__init_index()
+        self.__update_index(data)
+
+    @classmethod
+    def from_csv_file(cls, filename: str):
+        """
+        Create dictionary from csv file.
+        :param filename: CSV file name.
+        :return: Dictionary object.
+        """
+        with open(filename, "r", encoding="utf-8") as csv_file:
+            reader = csv.reader(csv_file)
+            return cls(reader)
 
     async def translate_spell_name(self, spell_name: str) -> Union[
             None,
